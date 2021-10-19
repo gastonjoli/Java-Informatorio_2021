a6d0b989f797f01bc94e1188b6df441d8b38df89
Gaston Jolinont <falsee63@gmail.com>
2021-10-19 09:33:28 -0300

    Complementarios

---
+++ src/EjerciciosComplementariosLevel1/Ejercicio9.java
@@ -0,0 +1,28 @@
+package EjerciciosComplementariosLevel1;
+  /* EJERCICIO 9:
+    Dado un String de entrada (frase, texto, etc)
+    calcular la cantidad de veces que aparece una letra dada.
+    */
+import java.util.Scanner;
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingrese una Oracion: ");
+        String texto = entrada.nextLine();
+        System.out.print("Que letra desea saber cuantas veces se repite?: ");
+        char caracter = entrada.next().charAt(0);
+
+        char[] letras = texto.toCharArray();
+        int cantidad = 0;
+
+        for (int i = 0; i < letras.length; i++){
+            if (letras[i] == caracter){
+                cantidad++;
+            }
+        }
+
+        System.out.println("La letra se repite "  + cantidad +  " veces");
+        entrada.close();
+    }
+}
